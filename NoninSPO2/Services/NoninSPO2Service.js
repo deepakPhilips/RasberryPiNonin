703ee4965e2bc7ae40618681626d665c1769ee32
deepakPhilips <98527237+deepakPhilips@users.noreply.github.com>
2025-04-14 11:56:34 +0530

    Removed write charectersitics

--- NoninSPO2/Services/NoninSPO2Service.js
+++ NoninSPO2/Services/NoninSPO2Service.js
@@ -6,20 +6,16 @@ var NoninSPO2_NotifyCharacteristic=require('./characteristics/NoninSPO2/NoninSPO
 // var NoninSPO2_WriteCharacteristic=require('./characteristics/NoninSPO2/NoninSPO2_WriteCharacteristic');
 
 
-let serviceUUIDs=require('../extra/Service_UUIDs.json');
-var data4=JSON.stringify(serviceUUIDs);
+let rawdata4=require('../extra/Service_UUIDs.json');
+var data4=JSON.stringify(rawdata4);
 let test4=JSON.parse(data4);
 
 function  NoninSPO2Service(){
 
 
 	bleno.PrimaryService.call(this,{
-		uuid:"46a970e00d5f11e28b5e0002a5d5c51b",
-		characteristics:[ new NoninSPO2_NotifyCharacteristic() ,
-                                    new NoninSPO2_WriteCharacteristic() 
-                               
-
-		         ]
+		uuid:test4["NoninSPO2"].uuid,
+		characteristics:[ new NoninSPO2_NotifyCharacteristic()]
 	  } );
 
 };
