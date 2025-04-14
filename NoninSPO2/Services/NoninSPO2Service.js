7223a0f516c73dc34c03a68a67a33f9414492b21
deepakPhilips <98527237+deepakPhilips@users.noreply.github.com>
2025-04-14 11:59:37 +0530

    Updated nonin service

--- NoninSPO2/Services/NoninSPO2Service.js
+++ NoninSPO2/Services/NoninSPO2Service.js
@@ -10,14 +10,14 @@ let rawdata4=require('../extra/Service_UUIDs.json');
 var data4=JSON.stringify(rawdata4);
 let test4=JSON.parse(data4);
 
-function  NoninSPO2Service(){
+const serviceUUIDs = require('../extra/Service_UUIDs.json'); // Directly load the JSON file
 
+function NoninSPO2Service() {
+  bleno.PrimaryService.call(this, {
+    uuid: "46a970e00d5f11e28b5e0002a5d5c51b",
+    characteristics: [new NoninSPO2_NotifyCharacteristic()],
+  });
+}
 
-	bleno.PrimaryService.call(this,{
-		uuid:"46a970e00d5f11e28b5e0002a5d5c51b",
-		characteristics:[ new NoninSPO2_NotifyCharacteristic()]
-	  } );
-
-};
 util.inherits(NoninSPO2Service,bleno.PrimaryService);
 module.exports=NoninSPO2Service;
