a040702ce6550fceeb8d7565426deb7adfe1c7b0
deepakPhilips <98527237+deepakPhilips@users.noreply.github.com>
2025-04-14 14:54:42 +0530

    updated systeminfom

--- NoninSPO2/Services/SystemInformationService.js
+++ NoninSPO2/Services/SystemInformationService.js
@@ -1,27 +1,24 @@
-var bleno = require('@abandonware/bleno');
-var util = require('util');
+const bleno = require('@abandonware/bleno');
+const util = require('util');
 
-var ManufacturerNameCharacteristic = require('./characteristics/SystemInformation/ManufacturerNameCharacteristic');
-var ModelNumberCharacteristic = require('./characteristics/SystemInformation/ModelNumberCharacteristic');
-var SerialNumberCharacteristic = require('./characteristics/SystemInformation/SerialNumberCharacteristic');
+const ManufacturerNameCharacteristic = require('./characteristics/SystemInformation/ManufacturerNameCharacteristic');
+const ModelNumberCharacteristic = require('./characteristics/SystemInformation/ModelNumberCharacteristic');
+const SerialNumberCharacteristic = require('./characteristics/SystemInformation/SerialNumberCharacteristic');
 
+const rawdata4 = require('../../MIScale/extra/Service_UUIDs.json');
+const test4 = JSON.parse(JSON.stringify(rawdata4));
 
-let rawdata4=require('../../MIScale/extra/Service_UUIDs.json');
-var data4=JSON.stringify(rawdata4);
-let test4=JSON.parse(data4);
+class SystemInformationService extends bleno.PrimaryService {
+  constructor() {
+    super({
+      uuid: test4["Sys"].uuid,
+      characteristics: [
+        new ManufacturerNameCharacteristic(),
+        new SerialNumberCharacteristic(),
+        new ModelNumberCharacteristic(),
+      ],
+    });
+  }
+}
 
-
-function SystemInformationService() {
-
-  bleno.PrimaryService.call(this, {
-   uuid: test4["Sys"].uuid,
-    characteristics: [
-      new ManufacturerNameCharacteristic(),
-      new SerialNumberCharacteristic(),
-      new ModelNumberCharacteristic()
-    ]
-  });
-};
-
-util.inherits(SystemInformationService, bleno.PrimaryService);
 module.exports = SystemInformationService;
