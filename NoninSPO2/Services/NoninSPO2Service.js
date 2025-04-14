c35f3955baa19a0329adc0311c60f8cd59fa3a50
deepakPhilips <98527237+deepakPhilips@users.noreply.github.com>
2025-04-14 11:55:32 +0530

    Updated service UUID

--- NoninSPO2/Services/NoninSPO2Service.js
+++ NoninSPO2/Services/NoninSPO2Service.js
@@ -6,15 +6,15 @@ var NoninSPO2_NotifyCharacteristic=require('./characteristics/NoninSPO2/NoninSPO
 // var NoninSPO2_WriteCharacteristic=require('./characteristics/NoninSPO2/NoninSPO2_WriteCharacteristic');
 
 
-let rawdata4=require('../extra/Service_UUIDs.json');
-var data4=JSON.stringify(rawdata4);
+let serviceUUIDs=require('../extra/Service_UUIDs.json');
+var data4=JSON.stringify(serviceUUIDs);
 let test4=JSON.parse(data4);
 
 function  NoninSPO2Service(){
 
 
 	bleno.PrimaryService.call(this,{
-		uuid:test4["NoninSPO2"].uuid,
+		uuid:"46a970e00d5f11e28b5e0002a5d5c51b",
 		characteristics:[ new NoninSPO2_NotifyCharacteristic() ,
                                     new NoninSPO2_WriteCharacteristic() 
                                
