5d669f5028d0f0f4b8044e94198b175008a91ad6
Deepak <deepak.mk@philips.com>
2025-04-15 23:25:00 +0530

    Updated Thermometer services

--- OximeterPhilips/Thermometer/Thermometer.js
+++ OximeterPhilips/Thermometer/Thermometer.js
@@ -1,14 +1,16 @@
 var bleno = require('@abandonware/bleno');
 var program = require('commander').program;
+
+// Load device configuration
 var deviceConfig = require('./thermometerConfig.json');
+
 var { createPrimaryService } = require('./ThermometerService');
 var {
     createCharacteristic,
-    createNotifyCharacteristicWithRead,
+    createNotifyCharacteristic,
 } = require('./ThermometerCharacteristic');
 
 var counter = 0;
-var measurementIntervalId = null;
 
 program
     .requiredOption('-t, --temperature <n>', 'temperature', parseFloat)
@@ -16,83 +18,88 @@ program
 
 const options = program.opts();
 
-bleno.on('stateChange', (state) => {
+bleno.on('stateChange', handleStateChange);
+bleno.on('accept', handleAccept);
+bleno.on('disconnect', handleDisconnect);
+bleno.on('advertisingStart', handleAdvertisingStart);
+
+function handleStateChange(state) {
     console.log('GATT thermometer server running');
     console.log('Temperature value: %j', options.temperature);
-
     if (state === 'poweredOn') {
-        bleno.setServices([
-            createPrimaryService('180A', [
-                createCharacteristic('2A29', ['read'], deviceConfig.manufacturer, 'Manufacturer Name'),
-                createCharacteristic('2A24', ['read'], deviceConfig.model, 'Model'),
-                createCharacteristic('2A25', ['read'], 'thermo_sim', 'Serial'),
-                createCharacteristic('2A28', ['read'], 'v1.0', 'Software Revision'),
-                createCharacteristic('2A26', ['read'], 'Firmware v1.0', 'Firmware Revision'),
-            ]),
-            createPrimaryService(deviceConfig.broadcastingServiceID, [
-                createNotifyCharacteristicWithRead(
-                    deviceConfig.characteristicID,
-                    'Temperature Measurement',
-                    handleMeasurementSubscribe,
-                    handleMeasurementUnsubscribe,
-                    options.temperature
-                ),
-            ]),
-        ], (err) => {
-            if (err) {
-                console.error('Error setting services:', err);
-                return;
-            }
-
-            bleno.startAdvertising("Tem BH 0x1", [
-                deviceConfig.readingServiceID // advertise only 1809
-            ]);
-        });
+        bleno.startAdvertising(deviceConfig.broadcastingName, [
+            deviceConfig.readingServiceID,
+            deviceConfig.broadcastingServiceID,
+        ]);
     } else {
         bleno.stopAdvertising();
     }
-});
+}
 
-bleno.on('accept', (clientAddress) => {
+function handleAccept(clientAddress) {
     console.log('Connected to: ' + clientAddress);
-});
+}
 
-bleno.on('disconnect', () => {
+function handleDisconnect() {
     console.log('Disconnected');
-});
+}
+
+function handleAdvertisingStart(error) {
+    if (error) {
+        console.error('Error starting advertising:', error);
+        return;
+    }
+
+    console.log('Started advertising');
+    bleno.setServices([
+        createPrimaryService('180A', [
+            createCharacteristic('2A29', ['read'], deviceConfig.manufacturer, 'Manufacturer Name'),
+            createCharacteristic('2A24', ['read'], deviceConfig.model, 'Model'),
+            createCharacteristic('2A25', ['read'], 'thermo_sim', 'Serial'),
+            createCharacteristic('2A28', ['read'], 'v1.0', 'Software Revision'),
+            createCharacteristic('2A26', ['read'], 'Firmware v1.0', 'Firmware Revision'),
+        ]),
+        createPrimaryService(deviceConfig.readingServiceID, [
+            createNotifyCharacteristic(
+                deviceConfig.characteristicID,
+                'Temperature Measurement',
+                handleMeasurementSubscribe,
+                handleMeasurementUnsubscribe
+            ),
+        ]),
+    ]);
+}
 
 function handleMeasurementSubscribe(maxValueSize, updateValueCallback) {
     console.log('Device subscribed, sending temperature measurements');
+    if (isValidMeasurement(options.temperature)) {
+        const intervalId = setInterval(() => {
+            const measBuffer = processMeasurement();
+            console.log('Sending measurement:', measBuffer);
+            updateValueCallback(measBuffer);
+        }, 1000); // Send data every 1 second
 
-    if (!isValidMeasurement(options.temperature)) {
-        console.error("Invalid temperature range.");
-        process.exit(2);
+        // Store the interval ID to clear it later
+        this.intervalId = intervalId;
+    } else {
+        console.error('Invalid measurement');
     }
-
-    measurementIntervalId = setInterval(() => {
-        const measBuffer = ieee11073Float(options.temperature);
-        console.log('Sending measurement:', measBuffer);
-        updateValueCallback(measBuffer);
-    }, 1000);
 }
 
 function handleMeasurementUnsubscribe() {
     console.log('Measurement unsubscribed');
-    clearInterval(measurementIntervalId);
-    measurementIntervalId = null;
+    // Clear the interval to stop sending data
+    clearInterval(this.intervalId);
 }
 
-function isValidMeasurement(temp) {
-    return temp > 35 && temp < 42;
+function isValidMeasurement(temperature) {
+    return temperature > 35 && temperature < 42; // Valid human body temperature range
 }
 
-function ieee11073Float(tempCelsius) {
-    const flags = 0x00;
-    const exponent = 0xFE; // -2
-    const mantissa = Math.round(tempCelsius * 100); // e.g., 37.5°C -> 3750
-    const ieee = (exponent << 24) | (mantissa & 0x00FFFFFF);
-    const buffer = Buffer.alloc(5);
-    buffer.writeUInt8(flags, 0);
-    buffer.writeInt32LE(ieee, 1);
-    return buffer;
+function processMeasurement() {
+    counter++;
+    const tempHex = Math.round(options.temperature * 100).toString(16).padStart(4, '0');
+    const temp1 = parseInt(tempHex.slice(0, 2), 16);
+    const temp2 = parseInt(tempHex.slice(2, 4), 16);
+    return [0x0a, 0x15, 0x1e, 0x00, counter, temp1, temp2];
 }
--- OximeterPhilips/Thermometer/ThermometerCharacteristic.js
+++ OximeterPhilips/Thermometer/ThermometerCharacteristic.js
@@ -6,38 +6,22 @@ function createCharacteristic(uuid, properties, value, descriptorValue) {
     return new Characteristic({
         uuid,
         properties,
-        value: Buffer.from(String(value), 'utf-8'),
+        value: Buffer.from(value),
         descriptors: [new Descriptor({ uuid: '2901', value: descriptorValue })],
     });
 }
 
-function createNotifyCharacteristicWithRead(uuid, descriptorValue, onSubscribe, onUnsubscribe, tempValue) {
+function createNotifyCharacteristic(uuid, descriptorValue, onSubscribe, onUnsubscribe) {
     return new Characteristic({
         uuid,
-        properties: ['read', 'notify'],
+        properties: ['notify'],
         descriptors: [new Descriptor({ uuid: '2901', value: descriptorValue })],
         onSubscribe,
         onUnsubscribe,
-        onReadRequest: (offset, callback) => {
-            const buffer = ieee11073Float(tempValue);
-            console.log('Read request: sending', buffer);
-            callback(Characteristic.RESULT_SUCCESS, buffer);
-        },
     });
-}
-
-function ieee11073Float(tempCelsius) {
-    const flags = 0x00;
-    const exponent = 0xFE;
-    const mantissa = Math.round(tempCelsius * 100);
-    const ieee = (exponent << 24) | (mantissa & 0x00FFFFFF);
-    const buffer = Buffer.alloc(5);
-    buffer.writeUInt8(flags, 0);
-    buffer.writeInt32LE(ieee, 1);
-    return buffer;
 }
 
 module.exports = {
     createCharacteristic,
-    createNotifyCharacteristicWithRead,
+    createNotifyCharacteristic,
 };
