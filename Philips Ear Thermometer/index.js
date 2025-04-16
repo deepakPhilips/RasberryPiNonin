2f2ddaa7779dccb819ddba82e7e92c428b18ff79
Deepak <deepak.mk@philips.com>
2025-04-16 18:59:11 +0530

    Updated Philips ear thermometer

--- Philips Ear Thermometer/index.js
+++ Philips Ear Thermometer/index.js
@@ -1,5 +1,4 @@
-var bleno = require('bleno');
-var readline=require('readline-sync');
+var bleno = require('@abandonware/bleno');
 
 var SystemInformationService = require('./Services/SystemInformationService');
 
@@ -9,23 +8,21 @@ var systemInformationService = new SystemInformationService();
 var BatteryService=require('./Services/BatteryService');
 
 var batteryService=new BatteryService();
-var Logger=require('/home/pi/Documents/RaspberryPi_Project/Logger.js');
  
-var logger=new Logger();
 
 var HealthThermometerService=require('./Services/HealthThermometerService');
 
 var healthThermometerService=new HealthThermometerService();
 
 
-let rawdata1=require('/home/pi/Documents/RaspberryPi_Project/Philips Ear Thermometer/extra/ConsoleLogComments.json');
+let rawdata1=require('./extra/ConsoleLogComments.json');
 var data1=JSON.stringify(rawdata1);
 let test1=JSON.parse(data1);
 
 
 
 bleno.on('stateChange', function(state) {
-  logger.log('on -> stateChange: ' + state);
+  console.log('on -> stateChange: ' + state);
 
 
   if (state === 'poweredOn') {
@@ -42,13 +39,13 @@ bleno.on('stateChange', function(state) {
 
 bleno.on ('advertisingStop', function (error)
 {
-    logger.log ( 'Advertising Stopped …');
+  console.log ( 'Advertising Stopped …');
 });
 
 
 bleno.on('advertisingStart', function(error) {
 
-  logger.log('on -> advertisingStart: ' +
+  console.log('on -> advertisingStart: ' +
     (error ? 'error ' + error : 'success')
   );
 
