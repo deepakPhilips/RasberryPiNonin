71fd1908f6f4688432962516c858af490f826a48
Deepak <deepak.mk@philips.com>
2025-04-19 17:55:07 +0530

    Updated Config file

--- PhilipsDevices/BroadcastingDevice/BroadcastingDevice.js
+++ PhilipsDevices/BroadcastingDevice/BroadcastingDevice.js
@@ -1,98 +1,109 @@
 const bleno = require('@abandonware/bleno');
 const program = require('commander').program;
-const fs = require('fs');
 
 const { createPrimaryService } = require('./BroadcastingDeviceService');
 const {
-  createCharacteristic,
-  createNotifyCharacteristic,
+    createCharacteristic,
+    createNotifyCharacteristic,
 } = require('./BroadcastingDeviceCharacteristic');
 const {
-  handleMeasurementSubscribe,
-  handleMeasurementUnsubscribe,
+    handleMeasurementSubscribe,
+    handleMeasurementUnsubscribe,
 } = require('./MeasurementHandlers');
-const { loadDeviceConfig } = require('./DeviceConfigLoader');
+const { loadDeviceById } = require('./DeviceConfigLoader');
 
+// Parse command-line arguments
 program
-  .requiredOption('--oximeter <n>', 'oximeter', parseInt)
-  .option('--saturation <n>', 'saturation', parseInt)
-  .option('--pulse <n>', 'pulse', parseInt)
-  .option('--temperature <n>', 'temperature', parseFloat);
+    .requiredOption('--deviceId <n>', 'deviceId', parseInt)
+    .option('--saturation <n>', 'saturation', parseInt)
+    .option('--pulse <n>', 'pulse', parseInt)
+    .option('--temperature <n>', 'temperature', parseFloat);
 
 program.parse(process.argv);
 const options = program.opts();
+const { deviceId } = options;
 
-const { oximeter } = options;
-
-// Load config based on --oximeter flag
-const deviceConfig = loadDeviceConfig(oximeter);
+// Load device configuration
+const deviceConfig = loadDeviceById(deviceId);
 const DEVICE_TYPE = deviceConfig.type;
 
-
-
-
-
+// Handle BLE state changes
 bleno.on('stateChange', (state) => {
-  console.log(`GATT ${DEVICE_TYPE.toLowerCase()} server running`);
-  if (state === 'poweredOn') {
-    bleno.startAdvertising(deviceConfig.broadcastingName, [
-      '180A',
-      deviceConfig.broadcastingServiceID.toLowerCase().replace(/-/g, ''),
-    ]);
-  } else {
-    bleno.stopAdvertising();
-  }
+    console.log(`GATT ${DEVICE_TYPE.toLowerCase()} server running`);
+    if (state === 'poweredOn') {
+        bleno.startAdvertising(deviceConfig.broadcastingName, [
+            '180A',
+            deviceConfig.broadcastingServiceID.toLowerCase().replace(/-/g, ''),
+        ]);
+    } else {
+        bleno.stopAdvertising();
+    }
 });
 
+// Handle client connection
 bleno.on('accept', (clientAddress) => {
-  console.log('Connected to:', clientAddress);
+    console.log('Connected to:', clientAddress);
 });
 
+// Handle client disconnection
 bleno.on('disconnect', () => {
-  console.log('Disconnected');
-  process.exit(0);
+    console.log('Disconnected');
+    process.exit(0);
 });
 
+// Handle advertising start
 bleno.on('advertisingStart', (error) => {
-  if (error) {
-    console.error('Advertising error:', error);
-    return;
-  }
-  console.log('Started advertising');
+    if (error) {
+        console.error('Advertising error:', error);
+        return;
+    }
 
-  const measurementChar = createNotifyCharacteristic(
-    deviceConfig.characteristicID.toLowerCase().replace(/-/g, ''),
-    DEVICE_TYPE === 'Pulse Oximeter' ? 'Measurement' : 'Temperature Measurement',
-    handleMeasurementSubscribe.bind(null, options, DEVICE_TYPE),
-    handleMeasurementUnsubscribe
-  );
+    console.log('Started advertising');
 
-  const services = [
-    createPrimaryService('180A', [
-      createCharacteristic('2A29', ['read'], deviceConfig.manufacturer, 'Manufacturer Name'),
-      createCharacteristic('2A24', ['read'], deviceConfig.model, 'Model'),
-      createCharacteristic('2A25', ['read'], 'sim_serial', 'Serial'),
-      createCharacteristic('2A28', ['read'], '1.0.0', 'Software Revision'),
-      createCharacteristic('2A26', ['read'], '1.0.0', 'Firmware Revision'),
-    ]),
-    createPrimaryService(deviceConfig.readingServiceID.toLowerCase().replace(/-/g, ''), [
-      measurementChar,
-    ]),
-  ];
+    // Create measurement characteristic
+    const measurementChar = createNotifyCharacteristic(
+        deviceConfig.characteristicID.toLowerCase().replace(/-/g, ''),
+        DEVICE_TYPE === 'Pulse Oximeter' ? 'Measurement' : 'Temperature Measurement',
+        handleMeasurementSubscribe.bind(null, options, DEVICE_TYPE),
+        handleMeasurementUnsubscribe
+    );
 
-  if (DEVICE_TYPE === 'Thermometer') {
-    services.push(createPrimaryService('1523', [
-      new bleno.Characteristic({
-        uuid: '1524',
-        properties: ['write'],
-        descriptors: [new bleno.Descriptor({ uuid: '2901', value: 'Foracare Serial Command' })],
-        onWriteRequest: (data, offset, withoutResponse, callback) => {
-          console.log('[Foracare Command] Received:', data.toString('hex'));
-          callback(bleno.Characteristic.RESULT_SUCCESS);
-        },
-      }),
-    ]));
-  }
+    // Define services
+    const services = [
+        createPrimaryService('180A', [
+            createCharacteristic('2A29', ['read'], deviceConfig.manufacturer, 'Manufacturer Name'),
+            createCharacteristic('2A24', ['read'], deviceConfig.model, 'Model'),
+            createCharacteristic('2A25', ['read'], 'sim_serial', 'Serial'),
+            createCharacteristic('2A28', ['read'], '1.0.0', 'Software Revision'),
+            createCharacteristic('2A26', ['read'], '1.0.0', 'Firmware Revision'),
+        ]),
+        createPrimaryService(deviceConfig.readingServiceID.toLowerCase().replace(/-/g, ''), [
+            measurementChar,
+        ]),
+    ];
 
-  bleno.setServices(services);
+    // Add thermometer-specific service if applicable
+    if (DEVICE_TYPE === 'Thermometer') {
+        services.push(
+            createPrimaryService('1523', [
+                new bleno.Characteristic({
+                    uuid: '1524',
+                    properties: ['write'],
+                    descriptors: [
+                        new bleno.Descriptor({
+                            uuid: '2901',
+                            value: 'Foracare Serial Command',
+                        }),
+                    ],
+                    onWriteRequest: (data, offset, withoutResponse, callback) => {
+                        console.log('[Foracare Command] Received:', data.toString('hex'));
+                        callback(bleno.Characteristic.RESULT_SUCCESS);
+                    },
+                }),
+            ])
+        );
+    }
+
+    // Set services
+    bleno.setServices(services);
 });
--- PhilipsDevices/BroadcastingDevice/DeviceConfigLoader.js
+++ PhilipsDevices/BroadcastingDevice/DeviceConfigLoader.js
@@ -1,10 +1,14 @@
-function loadDeviceConfig(oximeter) {
-  const configPath = oximeter == 1
-    ? './OximeterDeviceConfig.json'
-    : './ThermometerDeviceConfig.json';
-  return require(configPath);
+const fs = require('fs');
+const path = require('path');
+
+function loadDeviceById(id) {
+  const filePath = path.join(__dirname, 'devices_no_pairing.json');
+  const devices = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  return devices.find((device) => device.id === id) || null;
 }
 
+
 module.exports = {
-  loadDeviceConfig,
+    loadDeviceById,
 };
