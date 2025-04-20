d87694d4e9a8b61a084bfa964f28ae585519bbbe
Deepak <deepak.mk@philips.com>
2025-04-20 11:26:44 +0530

    UPdated path

--- PhilipsDevices/HeartRateMonitor/HeartRatePeripheral.js
+++ PhilipsDevices/HeartRateMonitor/HeartRatePeripheral.js
@@ -1,15 +1,6 @@
 const bleno = require('@abandonware/bleno');
 const deviceConfig = require('./HeartRateDeviceConfig.json');
 const { createHeartRateCharacteristic } = require('./HeartRateCharacteristic');
-const { execSync } = require('child_process');
-
-// Set MAC address before advertising
-try {
-  console.log('🛠️  Running set_mac.sh to update Bluetooth MAC...');
-  execSync('bash ./../set_mac.sh', { stdio: 'inherit' });
-} catch (error) {
-  console.error('❌ Failed to set MAC address:', error.message);
-}
 
 bleno.on('stateChange', (state) => {
   console.log('💓 HRM BLE State:', state);
