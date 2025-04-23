6cdc9aa814eaa4fabdb534edc8895cc2e3711538
Deepak <deepak.mk@philips.com>
2025-04-23 20:24:30 +0530

    Removed unnessary data

--- PhilipsDevices/BroadcastingDevice/PairableDevice.js
+++ PhilipsDevices/BroadcastingDevice/PairableDevice.js
@@ -155,7 +155,7 @@ const deviceInfoService = new bleno.PrimaryService({
 
 bleno.on('stateChange', (state) => {
   if (state === 'poweredOn') {
-    bleno.startAdvertising(deviceConfig.broadcastingName, ['180A',WEIGHT_SERVICE_UUID]);
+    bleno.startAdvertising(deviceConfig.broadcastingName, [WEIGHT_SERVICE_UUID]);
   } else {
     bleno.stopAdvertising();
   }
