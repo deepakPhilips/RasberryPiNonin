3f213e79a7096457d1a562ddb7dbbca5ebb549a0
Deepak <deepak.mk@philips.com>
2025-04-24 16:33:34 +0530

    added new Device

--- PhilipsDevices/BroadcastingDevice/BroadcastingDevice.js
+++ PhilipsDevices/BroadcastingDevice/BroadcastingDevice.js
@@ -89,7 +89,7 @@ async function promptIfNeeded() {
         console.log(`GATT ${DEVICE_TYPE.toLowerCase()} server running`);
         if (state === 'poweredOn') {
             console.log("🚀 ~ bleno.on ~ deviceConfig.broadcastingName:", deviceConfig.broadcastingName);
-            bleno.startAdvertising('A&D_UC-352BLE_AA26F0', [
+            bleno.startAdvertising(deviceConfig.broadcastingName, [
                 '180A',
                 deviceConfig.broadcastingServiceID.toLowerCase().replace(/-/g, ''),
             ]);
